3c68bdc9befbd26b20934106b18c75697792f535
alexandra-burca <alexandra.h.burca@gmail.com>
2024-01-10 00:47:52 +0200

    import change

--- main.py
+++ main.py
@@ -2,23 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 import timm
-
-import matplotlib.pyplot as plt  # For data viz
-import pandas as pd
-import numpy as np
-import sys
 from tqdm import tqdm
-
-
-# print('System Version:', sys.version)
-# print('PyTorch version', torch.__version__)
-# print('Torchvision version', torchvision.__version__)
-# print('Numpy version', np.__version__)
-# print('Pandas version', pd.__version__)
 
 
 class PlayingCardDataset(Dataset):
@@ -37,7 +24,6 @@ class PlayingCardDataset(Dataset):
 
 
 data_dir = 'train'
-# target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
 
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
@@ -134,6 +120,3 @@ for epoch in range(num_epochs):
     val_loss = running_loss / len(val_loader.dataset)
     val_losses.append(val_loss)
     print(f"Epoch {epoch + 1}/{num_epochs} - Train loss: {train_loss}, Validation loss: {val_loss}")
-
-
-
