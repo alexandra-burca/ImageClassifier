e9577221860e442bd14a4770f32bc06344c71f5a
alexandra-burca <alexandra.h.burca@gmail.com>
2024-01-10 00:34:47 +0200

    code updates

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ import matplotlib.pyplot as plt  # For data viz
 import pandas as pd
 import numpy as np
 import sys
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
-print('System Version:', sys.version)
-print('PyTorch version', torch.__version__)
-print('Torchvision version', torchvision.__version__)
-print('Numpy version', np.__version__)
-print('Pandas version', pd.__version__)
+
+# print('System Version:', sys.version)
+# print('PyTorch version', torch.__version__)
+# print('Torchvision version', torchvision.__version__)
+# print('Numpy version', np.__version__)
+# print('Pandas version', pd.__version__)
 
 
 class PlayingCardDataset(Dataset):
@@ -35,10 +36,8 @@ class PlayingCardDataset(Dataset):
         return self.data.classes
 
 
-# Get a dictionary associating target values with folder names
 data_dir = 'train'
-target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
-# print(target_to_class)
+# target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
 
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
@@ -46,7 +45,95 @@ transform = transforms.Compose([
 ])
 
 dataset = PlayingCardDataset(data_dir, transform)
+for image, label in dataset:
+    break
 
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+for images, labels in dataloader:
+    break
+
+
+class SimpleCardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+model = SimpleCardClassifier(num_classes=53)
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+train_folder = 'train'
+valid_folder = 'valid'
+test_folder = 'test'
+
+train_dataset = PlayingCardDataset(train_folder, transform=transform)
+val_dataset = PlayingCardDataset(valid_folder, transform=transform)
+test_dataset = PlayingCardDataset(test_folder, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+# Simple training loop
+num_epochs = 5
+train_losses, val_losses = [], []
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = SimpleCardClassifier(num_classes=53)
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(num_epochs):
+    # Training phase
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc="Training loop", dynamic_ncols=True):
+        # Move inputs and labels to the device
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * labels.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    # Validation phase
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader, desc='Validation loop', dynamic_ncols=True):
+            # Move inputs and labels to the device
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * labels.size(0)
+    val_loss = running_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+    print(f"Epoch {epoch + 1}/{num_epochs} - Train loss: {train_loss}, Validation loss: {val_loss}")
 
 
 
